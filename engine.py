6be78d2bc5d3ae5a76326490f039c8e4193de5e2
enrico8055 <54464654+enrico8055@users.noreply.github.com>
2021-05-22 20:55:19 +0700

    Update engine.py

--- engine.py
+++ engine.py
@@ -3,6 +3,7 @@ from nltk.stem import WordNetLemmatizer
 import json, string, numpy, random
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 def engine(userInput):
     #ambil data pattern response dari json
@@ -40,13 +41,25 @@ def engine(userInput):
 
     #hitung cosin antara setiap userinput dan pattern unutk dapatkan response
     for z, x in enumerate(data['data']):
-        #buat vocab       
+        #remove stopword     
         data['data'][z]['pattern'] = stopword_removal(data['data'][z]['pattern']) #pattern hapus stopword
+        
+        #lematization
         def lemma(token):
             lemmatizer = WordNetLemmatizer()    
             return [lemmatizer.lemmatize(t) for t in token]
         userInput = lemma(userInput)
         data['data'][z]['pattern'] = lemma(data['data'][z]['pattern'])
+
+        #stemming
+        def stemming(token):
+            ps = PorterStemmer()
+            return [ps.stem(t) for t in token]
+        userInput = stemming(userInput)
+        data['data'][z]['pattern'] = stemming(data['data'][z]['pattern'])
+        
+        
+        #buat vocab  
         vocab = userInput + data['data'][z]['pattern']
         vocab = list(dict.fromkeys(vocab)) #hilangkan duplicate
 
@@ -92,4 +105,3 @@ def engine(userInput):
     #ambil response secara random
     r = random.randint(0, len(data['data'][hasil['Index']]['response'])-1)
     return data['data'][hasil['Index']]['response'][r]
-
